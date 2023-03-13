2820071ca12d2a65b4fc793d4fc3b113b682a89d
BryanTavarez <bryanrtd@hotmail.com>
2023-03-13 09:16:47 -0400

    **Customer-service** Added missing methods to customer service controller

--- customer-service/src/main/java/com/microservice/customerservice/controller/CustomerRest.java
+++ customer-service/src/main/java/com/microservice/customerservice/controller/CustomerRest.java
@@ -1,20 +1,33 @@
 package com.microservice.customerservice.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.BindingResultUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservice.customerservice.entity.Customer;
 import com.microservice.customerservice.entity.Region;
 import com.microservice.customerservice.service.CustomerService;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -56,6 +69,72 @@ public class CustomerRest {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(customer);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer, BindingResult result){
+        log.info("Creating Customer: {}", customer);
+        if (result.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
+        }
+
+        Customer customerDB = customerService.createCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerDB);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") Long id, @RequestBody Customer customer){
+        log.info("Updating Customer with id {}", id);
+
+        Customer currenCustomer = customerService.getCustomer(id);
+
+        if (null == currenCustomer) {
+            log.error("Unable to update. Customer with id {} not found.", id);
+            return ResponseEntity.notFound().build();
+        }
+
+        customer.setId(id);
+        currenCustomer = customerService.updateCustomer(customer);
+        return ResponseEntity.ok(currenCustomer);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") Long id){
+        log.info("Fetching & deleteing Customer with id {}", id);
+
+        Customer customer = customerService.getCustomer(id);
+        if (null == customer) {
+            log.error("Unable to delete. Customer with id {} not found.", id);
+            return ResponseEntity.notFound().build();
+        }
+
+        customer = customerService.deleteCustomer(customer);
+        return ResponseEntity.ok(customer);
+    }
+
+
+    private String formatMessage(BindingResult result){
+        List<Map<String, String>> errors = result.getFieldErrors().stream()
+            .map(err -> {
+                Map<String, String> error = new HashMap<>();
+                error.put(err.getField(), err.getDefaultMessage());
+                return error;
+            }).collect(Collectors.toList());
+
+        ErrorMessage errorMessage = ErrorMessage.builder()
+        .code("01")
+        .messages(errors)
+        .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonString;
     }
 
 }
